d42383bb56bf9f0b4be175a69836214f7484fa21
Valery Mironenko <vmironenko@ttsandbox-024.nym1.placeiq.net>
2015-04-14 05:22:53 -0400

    update files

--- bild.sh
+++ bild.sh
@@ -1,4 +1,4 @@
-cd ./adserving-devops
+cd ../adserving-devops
  ansible-playbook -i ./inventory/infrastructure ./playbooks/build.yml --tags=adserver -s -e 'version=5.0.0-SNAPSHOT branch=modular-adserver' -v
  ansible-playbook -i ./inventory/infrastructure ./playbooks/publish.yml --tags=adserver -s -e 'version=5.0.0-SNAPSHOT branch=modular-adserver' -vv 
  
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-cd ./adserving-devops
+cd ../adserving-devops
 sudo -u jenkins ansible-playbook -i ./inventory/performance ./playbooks/deploy.yml --tags=adserver -u thumbtack -s -e 'version=5.0.0-SNAPSHOT branch=modular-adserver'  -l 10.1.43.60 -vvv
  
