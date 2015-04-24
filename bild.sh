ec9a88c82ed7d7d4d1ead14f0d03e1954711fda3
Valery Mironenko <vmironenko@ttsandbox-024.nym1.placeiq.net>
2015-04-23 23:43:49 -0400

    updated

--- bild.sh
+++ bild.sh
@@ -1,4 +1,4 @@
 cd ../modular-adserver
  ansible-playbook -i ./inventory/infrastructure ./playbooks/build.yml --tags=adserver -s -e 'version=5.0.0-SNAPSHOT branch=modular-adserver' -v
-# ansible-playbook -i ./inventory/infrastructure ./playbooks/publish.yml --tags=adserver -s -e 'version=5.0.0-SNAPSHOT branch=modular-adserver' -vv 
+ ansible-playbook -i ./inventory/infrastructure ./playbooks/publish.yml --tags=adserver -s -e 'version=5.0.0-SNAPSHOT branch=modular-adserver' -vv 
  
